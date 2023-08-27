f0073c7387009d4f91b37360c93e042c77f57f89
F4NT0 <fantolaboratorio@hotmail.com>
2023-08-27 16:30:39 -0300

    Update alpha-nvim plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,17 +2,35 @@ return {
   "goolord/alpha-nvim", -- official lazyvim plugin
   enabled = true,
   opts = function(_, opts)
+    local dashboard = require("alpha.themes.dashboard")
     local logo = [[
-
-
-
-        ______            __             _         
-       / ____/___ _____  / /_____ _   __(_)___ ___ 
-      / /_  / __ `/ __ \/ __/ __ \ | / / / __ `__ \
-     / __/ / /_/ / / / / /_/ /_/ / |/ / / / / / / /
-    /_/    \__,_/_/ /_/\__/\____/|___/_/_/ /_/ /_/                                                
-              [FANTO IDE INTO TERMINAL]              
+      >      ███████╗ █████╗ ███╗   ██╗████████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗       Z
+        >    ██╔════╝██╔══██╗████╗  ██║╚══██╔══╝██╔═══██╗██║   ██║██║████╗ ████║     Z
+          >  █████╗  ███████║██╔██╗ ██║   ██║   ██║   ██║██║   ██║██║██╔████╔██║   Z
+           > ██╔══╝  ██╔══██║██║╚██╗██║   ██║   ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║Z
+             ██║     ██║  ██║██║ ╚████║   ██║   ╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+             ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝                                           
+                               [FANTO IDE INTO TERMINAL]              
     ]]
-    opts.section.header.val = vim.split(logo, "\n", { trimempty = true })
+    opts.section.header.val = vim.split(logo, "\n")
+    dashboard.section.buttons.val = {
+      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+      dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    }
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.header.opts.hl = "AlphaHeader"
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 8
+    return dashboard
   end,
 }
