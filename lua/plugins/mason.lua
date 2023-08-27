fe8294895bfeff5415fca41cb8e8e168fc19b17f
F4NT0 <fantolaboratorio@hotmail.com>
2023-08-27 20:38:35 -0300

    added treesitter and mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "java",
+        "c_sharp",
+        "go",
+        "sql",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
